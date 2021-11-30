5f57a204511f70c083c8d9a5bb2f8eee0ebb3b10
Luiz Philippe <luizphilippep@gmail.com>
2021-11-30 15:36:33 -0300

    Interpreter module
    Working for bold, italic and headers

---
+++ src/lexinterpreter.py
@@ -0,0 +1,27 @@
+from re import sub, search
+from src.regex import *
+
+def parse_bold(text):
+  return sub(bold_token, bold_result, text)
+
+def parse_italic(text):
+  return sub(italic_token, italic_result, text)
+
+def parse_header(text):
+  lines = text.split('\n')
+
+  for i in range(len(lines)):
+    line = lines[i]
+    res = search(header_token, line)
+    if res != None:
+      header_type = len(res.group(1))
+      if(header_type <= 6):
+        lines[i] = sub(header_token, header_result(header_type), line)
+
+  return '\n'.join(lines)
+
+def parse_text(markdown):
+  markdown = parse_bold(markdown)
+  markdown = parse_italic(markdown)
+  markdown = parse_header(markdown)
+  return markdown
