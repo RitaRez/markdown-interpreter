519c61a489f785dadcd6beb06c46e59c198b5337
Luiz Philippe <luizphilippep@gmail.com>
2021-11-30 15:37:45 -0300

    Main execution

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# markdown_interpreter
+# Markdown Interpreter
+
+A simple markdown interpreter made with _python_.
+
+## Installing
+
+```pip3 install requirements.txt```
+
+## Running
+
+```python3 main.py markdown-file.md```
---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from tkhtmlview import HTMLLabel
+try:
+    import Tkinter as tk # for Python2
+except ImportError:
+    import tkinter as tk # for Python3
+
+from  src.lexinterpreter import parse_text
+
+def setup_window(name):
+  window = tk.Tk()
+  window.configure(background='white')
+  window.title(name)
+  return window
+
+def display_html(html, file_path):
+  root = setup_window(file_path.split('/')[-1])
+  my_label = HTMLLabel(root, html=html)
+  my_label.pack()
+  root.mainloop()
+
+def main():
+  with open(sys.argv[1]) as f:
+    markdown = f.read()
+    html = parse_text(markdown)
+    # print(html)
+    display_html(html, sys.argv[1])
+
+if __name__=="__main__":
+  main()
