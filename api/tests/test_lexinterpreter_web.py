8706e3d55a06c2b1597854b1ca8ca981ca997a16
ritarez <ritarezborges@gmail.com>
2021-12-09 19:46:44 -0300

    finished e2e testing

--- api/tests/test_lexinterpreter_web.py
+++ api/tests/test_lexinterpreter_web.py
@@ -20,7 +20,20 @@ class AcceptanceTest(unittest.TestCase):
     source_code_link = self.driver.find_element(By.XPATH, '//*[@id="page-header"]/p/a')
     self.assertEqual(source_code_link.get_attribute('href'), 'https://github.com/RitaRez/markdown-interpreter')
 
-  def test_converting_list_should_return_li_elements(self):
+  def test_converting_unordered_list_should_return_li_elements(self):
+    markdown_text_area = self.driver.find_element(By.ID, 'markdown-textarea')
+    markdown_text_area.send_keys('- elemento 1\n- elemento 2\n- elemento 3')
+    
+    convert_btn = self.driver.find_element(By.ID, 'preview-button')
+    convert_btn.click()
+
+    time.sleep(3)
+
+    list_elements = self.driver.find_element(By.ID, 'preview-container').find_element(By.TAG_NAME, 'ul').find_elements(By.TAG_NAME, 'li')
+    for idx, el in enumerate(list_elements):
+      self.assertEqual(el.text, 'elemento ' + str(idx+1))
+
+  def test_converting_ordered_list_should_return_li_elements(self):
     markdown_text_area = self.driver.find_element(By.ID, 'markdown-textarea')
     markdown_text_area.send_keys('1. elemento 1\n2. elemento 2\n3. elemento 3')
     
@@ -29,7 +42,7 @@ class AcceptanceTest(unittest.TestCase):
 
     time.sleep(3)
 
-    list_elements = self.driver.find_element(By.ID, 'preview-container').find_elements(By.TAG_NAME, 'li')
+    list_elements = self.driver.find_element(By.ID, 'preview-container').find_element(By.TAG_NAME, 'ol').find_elements(By.TAG_NAME, 'li')
     for idx, el in enumerate(list_elements):
       self.assertEqual(el.text, 'elemento ' + str(idx+1))
 
@@ -52,9 +65,9 @@ class AcceptanceTest(unittest.TestCase):
     third_header = converted_html.find_element(By.TAG_NAME, 'h3')
     self.assertEqual(third_header.text, 'Small title')
 
-  def test_converting_headers_should_return_htags(self):
+  def test_converting_bold_italic_and_strike_should_return_i_b_stags(self):
     markdown_text_area = self.driver.find_element(By.ID, 'markdown-textarea')
-    markdown_text_area.send_keys('# Big Title\n## Medium title\n### Small title')
+    markdown_text_area.send_keys('**Bold** of you to assume that this is *italic*, ~~actualy it is~~')
     
     convert_btn = self.driver.find_element(By.ID, 'preview-button')
     convert_btn.click()
@@ -62,13 +75,13 @@ class AcceptanceTest(unittest.TestCase):
     time.sleep(3)
 
     converted_html = self.driver.find_element(By.ID, 'preview-container')
-    first_header = converted_html.find_element(By.TAG_NAME, 'h1')
-    self.assertEqual(first_header.text, 'Big Title')
+    bold = converted_html.find_element(By.TAG_NAME, 'b')
+    self.assertEqual(bold.text, 'Bold')
     
-    second_header = converted_html.find_element(By.TAG_NAME, 'h2')
-    self.assertEqual(second_header.text, 'Medium title')
+    italic = converted_html.find_element(By.TAG_NAME, 'i')
+    self.assertEqual(italic.text, 'italic')
     
-    third_header = converted_html.find_element(By.TAG_NAME, 'h3')
-    self.assertEqual(third_header.text, 'Small title')
+    striked = converted_html.find_element(By.TAG_NAME, 's')
+    self.assertEqual(striked.text, 'actualy it is')
 
     
